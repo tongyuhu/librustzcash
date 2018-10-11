4de18c183f9572cd6d2644a4bfd700c71247b131
Jack Grigg <jack@z.cash>
2018-10-11 23:16:48 +0100

    impl Display for BlockHash and TxId

--- zcash_primitives/src/lib.rs
+++ zcash_primitives/src/lib.rs
@@ -4,12 +4,10 @@ extern crate lazy_static;
 extern crate blake2_rfc;
 extern crate byteorder;
 extern crate ff;
+extern crate hex;
 extern crate pairing;
 extern crate rand;
 extern crate sapling_crypto;
-
-#[cfg(test)]
-extern crate hex;
 
 use sapling_crypto::jubjub::JubjubBls12;
 
