8c299aa49ea42c3de8084890b8b3547940cf8051
Jack Grigg <jack@z.cash>
2018-10-12 18:24:25 +0100

    Parse compact blocks to find wallet transactions

--- zcash_client_backend/src/lib.rs
+++ zcash_client_backend/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate blake2_rfc;
 extern crate byteorder;
 extern crate chacha20_poly1305_aead;
+extern crate hex;
 extern crate pairing;
 extern crate protobuf;
 extern crate rand;
@@ -8,5 +9,8 @@ extern crate sapling_crypto;
 extern crate zcash_primitives;
 extern crate zip32;
 
+pub mod data;
 mod note_encryption;
 pub mod proto;
+pub mod wallet;
+pub mod welding_rig;
---
+++ zcash_client_backend/src/wallet.rs
@@ -0,0 +1,21 @@
+use pairing::bls12_381::{Bls12, Fr};
+use sapling_crypto::jubjub::{edwards, PrimeOrder};
+use zcash_primitives::transaction::TxId;
+
+use data::EncCiphertextFrag;
+
+pub struct WalletTx {
+    pub txid: TxId,
+    pub num_spends: usize,
+    pub num_outputs: usize,
+    pub shielded_outputs: Vec<WalletShieldedOutput>,
+}
+
+pub struct WalletShieldedOutput {
+    pub index: usize,
+    pub cmu: Fr,
+    pub epk: edwards::Point<Bls12, PrimeOrder>,
+    pub enc_ct: EncCiphertextFrag,
+    pub account: usize,
+    pub value: u64,
+}
