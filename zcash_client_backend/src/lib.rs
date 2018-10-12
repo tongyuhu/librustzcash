8499e23c7e15c73b4736db1a1b14d43e3c6a0be7
Jack Grigg <jack@z.cash>
2018-10-12 18:22:58 +0100

    Build protobufs for compact formats

--- zcash_client_backend/src/lib.rs
+++ zcash_client_backend/src/lib.rs
@@ -3,10 +3,4 @@
 //! `zcash_client_backend` contains Rust structs and traits for creating shielded Zcash
 //! light clients.
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod proto;
