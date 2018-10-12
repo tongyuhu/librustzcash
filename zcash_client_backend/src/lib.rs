b5b74a089cac07fa5802c573846836eac9fca012
Jack Grigg <jack@z.cash>
2018-10-12 18:22:58 +0100

    Build protobufs for compact formats

--- zcash_client_backend/src/lib.rs
+++ zcash_client_backend/src/lib.rs
@@ -3,9 +3,11 @@ extern crate byteorder;
 extern crate chacha20_poly1305_aead;
 extern crate ff;
 extern crate pairing;
+extern crate protobuf;
 extern crate rand;
 extern crate sapling_crypto;
 extern crate zcash_primitives;
 extern crate zip32;
 
 mod note_encryption;
+pub mod proto;
