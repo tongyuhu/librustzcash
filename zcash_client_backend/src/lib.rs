8f9aabce1f52b6556e7df2594e2876e43c6bbc1e
Jack Grigg <jack@z.cash>
2018-11-15 15:48:04 +0000

    CompactBlock Stream using the JSON-RPC interface

--- zcash_client_backend/src/lib.rs
+++ zcash_client_backend/src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate failure;
+
 extern crate bech32;
 extern crate blake2_rfc;
 extern crate byteorder;
@@ -11,6 +14,23 @@ extern crate sapling_crypto;
 extern crate zcash_primitives;
 extern crate zip32;
 
+#[cfg(feature = "jsonrpc")]
+extern crate exonum_jsonrpc;
+
+#[cfg(feature = "jsonrpc")]
+extern crate hex_serde;
+
+#[cfg(feature = "jsonrpc")]
+extern crate serde;
+
+#[cfg(feature = "jsonrpc")]
+#[macro_use]
+extern crate serde_derive;
+
+#[cfg(feature = "jsonrpc")]
+#[macro_use]
+extern crate serde_json;
+
 pub mod address;
 pub mod constants;
 pub mod data;
@@ -19,3 +39,9 @@ mod note_encryption;
 pub mod proto;
 pub mod wallet;
 pub mod welding_rig;
+
+#[cfg(feature = "jsonrpc")]
+mod jsonrpc;
+
+#[cfg(feature = "jsonrpc")]
+pub use jsonrpc::RpcChainSync;
