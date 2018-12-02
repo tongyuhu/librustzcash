da2b719f4934a720e952989dd4584c17d41d2df4
Jack Grigg <jack@z.cash>
2018-12-02 17:26:38 +0000

    Implement PaymentAddress decoding

--- zcash_client_backend/src/address.rs
+++ zcash_client_backend/src/address.rs
@@ -1,7 +1,12 @@
 use bech32::{convert_bits, Bech32};
+use failure::Error;
 use pairing::bls12_381::Bls12;
-use sapling_crypto::primitives::PaymentAddress;
+use sapling_crypto::{
+    jubjub::edwards,
+    primitives::{Diversifier, PaymentAddress},
+};
 use std::io::Write;
+use zcash_primitives::JUBJUB;
 
 pub fn encode_payment_address(hrp: &str, addr: &PaymentAddress<Bls12>) -> String {
     let mut data: Vec<u8> = Vec::with_capacity(43);
@@ -18,6 +23,24 @@ pub fn encode_payment_address(hrp: &str, addr: &PaymentAddress<Bls12>) -> String
     encoded.to_string()
 }
 
+pub fn decode_payment_address(hrp: &str, s: &str) -> Result<PaymentAddress<Bls12>, Error> {
+    let encoded = Bech32::from_str_lenient(s)?;
+    if encoded.hrp() == hrp {
+        let data = convert_bits(encoded.data(), 5, 8, false)?;
+        let mut diversifier = Diversifier([0; 11]);
+        diversifier.0.copy_from_slice(&data[0..11]);
+        match edwards::Point::<Bls12, _>::read(&data[11..], &JUBJUB) {
+            Ok(p) => match p.as_prime_order(&JUBJUB) {
+                Some(pk_d) => Ok(PaymentAddress { pk_d, diversifier }),
+                None => Err(format_err!("pk_d is not prime order")),
+            },
+            Err(e) => Err(format_err!("{}", e)),
+        }
+    } else {
+        Err(format_err!("Invalid HRP"))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use pairing::bls12_381::Bls12;
@@ -28,7 +51,7 @@ mod tests {
     };
     use zcash_primitives::JUBJUB;
 
-    use super::encode_payment_address;
+    use super::{decode_payment_address, encode_payment_address};
     use constants;
 
     #[test]
@@ -40,13 +63,33 @@ mod tests {
             pk_d: edwards::Point::<Bls12, _>::rand(rng, &JUBJUB).mul_by_cofactor(&JUBJUB),
         };
 
+        let encoded_main =
+            "zs1qqqqqqqqqqqqqqqqqqxrrfaccydp867g6zg7ne5ht37z38jtfyw0ygmp0ja6hhf07twjqj2ug6x";
+        let encoded_test =
+            "ztestsapling1qqqqqqqqqqqqqqqqqqxrrfaccydp867g6zg7ne5ht37z38jtfyw0ygmp0ja6hhf07twjq6awtaj";
+
         assert_eq!(
             encode_payment_address(constants::HRP_SAPLING_EXTENDED_SPENDING_KEY_MAIN, &addr),
-            "zs1qqqqqqqqqqqqqqqqqqxrrfaccydp867g6zg7ne5ht37z38jtfyw0ygmp0ja6hhf07twjqj2ug6x"
+            encoded_main
         );
         assert_eq!(
+            decode_payment_address(
+                constants::HRP_SAPLING_EXTENDED_SPENDING_KEY_MAIN,
+                encoded_main
+            ).unwrap(),
+            addr
+        );
+
+        assert_eq!(
             encode_payment_address(constants::HRP_SAPLING_EXTENDED_SPENDING_KEY_TEST, &addr),
-            "ztestsapling1qqqqqqqqqqqqqqqqqqxrrfaccydp867g6zg7ne5ht37z38jtfyw0ygmp0ja6hhf07twjq6awtaj"
+            encoded_test
+        );
+        assert_eq!(
+            decode_payment_address(
+                constants::HRP_SAPLING_EXTENDED_SPENDING_KEY_TEST,
+                encoded_test
+            ).unwrap(),
+            addr
         );
     }
 }
