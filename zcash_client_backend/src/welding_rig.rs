12f6f9901faaf634755de846c54b22a0e3d90864
Jack Grigg <jack@z.cash>
2018-12-02 12:14:02 +0000

    Increment the commitment tree and witnesses while scanning blocks

--- zcash_client_backend/src/welding_rig.rs
+++ zcash_client_backend/src/welding_rig.rs
@@ -2,12 +2,16 @@ use ff::{PrimeField, PrimeFieldRepr};
 use pairing::bls12_381::{Bls12, Fr, FrRepr};
 use protobuf::parse_from_bytes;
 use sapling_crypto::jubjub::{edwards, fs::Fs, PrimeOrder};
-use zcash_primitives::{transaction::TxId, JUBJUB};
+use zcash_primitives::{
+    merkle_tree::{CommitmentTree, IncrementalWitness, Node},
+    transaction::TxId,
+    JUBJUB,
+};
 use zip32::ExtendedFullViewingKey;
 
 use data::EncCiphertextFrag;
 use note_encryption::try_sapling_compact_note_decryption;
-use proto::compact_formats::{CompactBlock, CompactOutput, CompactTx};
+use proto::compact_formats::{CompactBlock, CompactOutput};
 use wallet::{WalletShieldedOutput, WalletTx};
 
 fn trial_decrypt(
@@ -22,12 +26,17 @@ fn trial_decrypt(
     }
 }
 
-/// Returns a WalletShieldedOutput if this output belongs to any of the given
-/// ExtendedFullViewingKeys.
+/// Returns a WalletShieldedOutput and corresponding IncrementalWitness if this
+/// output belongs to any of the given ExtendedFullViewingKeys. The given
+/// CommitmentTree and existing IncrementalWitnesses are incremented with this
+/// output's commitment.
 fn scan_output(
     (index, output): (usize, CompactOutput),
     ivks: &[Fs],
-) -> Option<WalletShieldedOutput> {
+    tree: &mut CommitmentTree,
+    existing_witnesses: &mut [&mut IncrementalWitness],
+    new_witnesses: &mut [IncrementalWitness],
+) -> Option<(WalletShieldedOutput, IncrementalWitness)> {
     let mut repr = FrRepr::default();
     if repr.read_le(&output.cmu[..]).is_err() {
         return None;
@@ -47,6 +56,16 @@ fn scan_output(
 
     let ct = output.ciphertext;
 
+    // Increment tree and witnesses
+    let node = Node::new(cmu.into_repr());
+    for witness in existing_witnesses {
+        witness.append(node).unwrap();
+    }
+    for witness in new_witnesses {
+        witness.append(node).unwrap();
+    }
+    tree.append(node).unwrap();
+
     for (account, ivk) in ivks.iter().enumerate() {
         let value = match trial_decrypt(&cmu, &epk, &ct, ivk) {
             Some(value) => value,
@@ -57,63 +76,80 @@ fn scan_output(
         let mut enc_ct = EncCiphertextFrag([0u8; 52]);
         enc_ct.0.copy_from_slice(&ct);
 
-        return Some(WalletShieldedOutput {
-            index,
-            cmu,
-            epk,
-            enc_ct,
-            account,
-            value,
-        });
+        return Some((
+            WalletShieldedOutput {
+                index,
+                cmu,
+                epk,
+                enc_ct,
+                account,
+                value,
+            },
+            IncrementalWitness::from_tree(tree),
+        ));
     }
     None
 }
 
-/// Returns a WalletTx if this transaction belongs to any of the given
-/// ExtendedFullViewingKeys.
-fn scan_tx(tx: CompactTx, extfvks: &[ExtendedFullViewingKey]) -> Option<WalletTx> {
-    let num_spends = tx.spends.len();
-    let num_outputs = tx.outputs.len();
+/// Returns a vector of transactions belonging to any of the given
+/// ExtendedFullViewingKeys, and the corresponding new IncrementalWitnesses.
+/// The given CommitmentTree and existing IncrementalWitnesses are
+/// incremented appropriately.
+pub fn scan_block(
+    block: CompactBlock,
+    extfvks: &[ExtendedFullViewingKey],
+    tree: &mut CommitmentTree,
+    existing_witnesses: &mut [&mut IncrementalWitness],
+) -> Vec<(WalletTx, Vec<IncrementalWitness>)> {
+    let mut wtxs = vec![];
+    let ivks: Vec<_> = extfvks.iter().map(|extfvk| extfvk.fvk.vk.ivk()).collect();
 
-    // Check for incoming notes
-    let shielded_outputs: Vec<WalletShieldedOutput> = {
-        let ivks: Vec<_> = extfvks.iter().map(|extfvk| extfvk.fvk.vk.ivk()).collect();
-        tx.outputs
-            .into_iter()
-            .enumerate()
-            .filter_map(|(index, output)| scan_output((index, output), &ivks))
-            .collect()
-    };
+    for tx in block.vtx.into_iter() {
+        let num_spends = tx.spends.len();
+        let num_outputs = tx.outputs.len();
 
-    if shielded_outputs.is_empty() {
-        None
-    } else {
-        let mut txid = TxId([0u8; 32]);
-        txid.0.copy_from_slice(&tx.hash);
-        Some(WalletTx {
-            txid,
-            num_spends,
-            num_outputs,
-            shielded_outputs,
-        })
+        // Check for incoming notes while incrementing tree and witnesses
+        let mut shielded_outputs = vec![];
+        let mut new_witnesses = vec![];
+        for to_scan in tx.outputs.into_iter().enumerate() {
+            if let Some((output, new_witness)) =
+                scan_output(to_scan, &ivks, tree, existing_witnesses, &mut new_witnesses)
+            {
+                shielded_outputs.push(output);
+                new_witnesses.push(new_witness);
+            }
+        }
+
+        if !shielded_outputs.is_empty() {
+            let mut txid = TxId([0u8; 32]);
+            txid.0.copy_from_slice(&tx.hash);
+            wtxs.push((
+                WalletTx {
+                    txid,
+                    num_spends,
+                    num_outputs,
+                    shielded_outputs,
+                },
+                new_witnesses,
+            ));
+        }
     }
+
+    wtxs
 }
 
 /// Returns a vector of transactions belonging to any of the given
-/// ExtendedFullViewingKeys.
-pub fn scan_block(block: CompactBlock, extfvks: &[ExtendedFullViewingKey]) -> Vec<WalletTx> {
-    block
-        .vtx
-        .into_iter()
-        .filter_map(|tx| scan_tx(tx, extfvks))
-        .collect()
-}
-
-/// Returns a vector of transactions belonging to any of the given
-/// ExtendedFullViewingKeys.
-pub fn scan_block_from_bytes(block: &[u8], extfvks: &[ExtendedFullViewingKey]) -> Vec<WalletTx> {
+/// ExtendedFullViewingKeys, and the corresponding new IncrementalWitnesses.
+/// The given CommitmentTree and existing IncrementalWitnesses are
+/// incremented appropriately.
+pub fn scan_block_from_bytes(
+    block: &[u8],
+    extfvks: &[ExtendedFullViewingKey],
+    tree: &mut CommitmentTree,
+    witnesses: &mut [&mut IncrementalWitness],
+) -> Vec<(WalletTx, Vec<IncrementalWitness>)> {
     let block: CompactBlock =
         parse_from_bytes(block).expect("Cannot convert into a `CompactBlock`");
 
-    scan_block(block, extfvks)
+    scan_block(block, extfvks, tree, witnesses)
 }
