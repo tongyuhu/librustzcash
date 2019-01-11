0615f8335c30a50481393c797f689d649616d5be
Jack Grigg <jack@z.cash>
2019-01-11 14:26:54 -0800

    Detect change notes while scanning blocks

--- zcash_client_backend/src/welding_rig.rs
+++ zcash_client_backend/src/welding_rig.rs
@@ -5,6 +5,7 @@ use sapling_crypto::{
     jubjub::{edwards, fs::Fs, PrimeOrder},
     primitives::{Note, PaymentAddress},
 };
+use std::collections::HashSet;
 use zcash_primitives::{
     merkle_tree::{CommitmentTree, IncrementalWitness, Node},
     transaction::TxId,
@@ -35,6 +36,7 @@ fn trial_decrypt(
 fn scan_output(
     (index, output): (usize, CompactOutput),
     ivks: &[Fs],
+    spent_from_accounts: &HashSet<usize>,
     tree: &mut CommitmentTree,
     existing_witnesses: &mut [&mut IncrementalWitness],
     new_witnesses: &mut [IncrementalWitness],
@@ -74,6 +76,14 @@ fn scan_output(
             None => continue,
         };
 
+        // A note is marked as "change" if the account that received it
+        // also spent notes in the same transaction. This will catch,
+        // for instance:
+        // - Change created by spending fractions of notes.
+        // - Notes created by consolidation transactions.
+        // - Notes sent from one account to itself.
+        let is_change = spent_from_accounts.contains(&account);
+
         return Some((
             WalletShieldedOutput {
                 index,
@@ -82,6 +92,7 @@ fn scan_output(
                 account,
                 note,
                 to,
+                is_change,
             },
             IncrementalWitness::from_tree(tree),
         ));
@@ -96,7 +107,7 @@ fn scan_output(
 pub fn scan_block(
     block: CompactBlock,
     extfvks: &[ExtendedFullViewingKey],
-    nullifiers: &[&[u8]],
+    nullifiers: &[(&[u8], usize)],
     tree: &mut CommitmentTree,
     existing_witnesses: &mut [&mut IncrementalWitness],
 ) -> Vec<(WalletTx, Vec<IncrementalWitness>)> {
@@ -108,29 +119,45 @@ pub fn scan_block(
         let num_outputs = tx.outputs.len();
 
         // Check for spent notes
-        let shielded_spends: Vec<_> = tx
-            .spends
-            .into_iter()
-            .enumerate()
-            .filter_map(|(index, spend)| {
-                if nullifiers.contains(&&spend.nf[..]) {
-                    Some(WalletShieldedSpend {
-                        index,
-                        nf: spend.nf,
-                    })
-                } else {
-                    None
-                }
-            })
-            .collect();
+        let shielded_spends: Vec<_> =
+            tx.spends
+                .into_iter()
+                .enumerate()
+                .filter_map(|(index, spend)| {
+                    if let Some(account) = nullifiers.iter().find_map(|&(nf, acc)| {
+                        if nf == &spend.nf[..] {
+                            Some(acc)
+                        } else {
+                            None
+                        }
+                    }) {
+                        Some(WalletShieldedSpend {
+                            index,
+                            nf: spend.nf,
+                            account,
+                        })
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+        // Collect the set of accounts that were spent from in this transaction
+        let spent_from_accounts: HashSet<_> =
+            shielded_spends.iter().map(|spend| spend.account).collect();
 
         // Check for incoming notes while incrementing tree and witnesses
         let mut shielded_outputs = vec![];
         let mut new_witnesses = vec![];
         for to_scan in tx.outputs.into_iter().enumerate() {
-            if let Some((output, new_witness)) =
-                scan_output(to_scan, &ivks, tree, existing_witnesses, &mut new_witnesses)
-            {
+            if let Some((output, new_witness)) = scan_output(
+                to_scan,
+                &ivks,
+                &spent_from_accounts,
+                tree,
+                existing_witnesses,
+                &mut new_witnesses,
+            ) {
                 shielded_outputs.push(output);
                 new_witnesses.push(new_witness);
             }
@@ -162,7 +189,7 @@ pub fn scan_block(
 pub fn scan_block_from_bytes(
     block: &[u8],
     extfvks: &[ExtendedFullViewingKey],
-    nullifiers: &[&[u8]],
+    nullifiers: &[(&[u8], usize)],
     tree: &mut CommitmentTree,
     witnesses: &mut [&mut IncrementalWitness],
 ) -> Vec<(WalletTx, Vec<IncrementalWitness>)> {
