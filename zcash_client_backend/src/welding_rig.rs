775c456c2658a3b9bda0db430dfa3af0c6e2fc5a
Jack Grigg <jack@z.cash>
2019-01-24 23:26:14 +0000

    Add tx index within block to WalletTx struct

--- zcash_client_backend/src/welding_rig.rs
+++ zcash_client_backend/src/welding_rig.rs
@@ -163,6 +163,7 @@ pub fn scan_block(
             wtxs.push((
                 WalletTx {
                     txid,
+                    index: tx.index as usize,
                     num_spends,
                     num_outputs,
                     shielded_spends,
